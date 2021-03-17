bdbe40fd5c591df7d71a39f0ee8fd8f38176d7ec
somin1011 <leesomin1011@naver.com>
2021-03-17 11:52:28 +0900

    Add UserController [Delete]

--- UserController.java
+++ UserController.java
@@ -19,4 +19,11 @@ public class UserController {
      logger.info("경로 : update");
      return "user/update";
    } 
+
+ // 회원삭제 페이지로 이동
+   @RequestMapping("user/delete.do")
+    public String delete() {
+     logger.info("경로 : delete");
+     return "user/delete";
+   } 
 }
