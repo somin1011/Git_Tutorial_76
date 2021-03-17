10d808678c39f96f796a77ad81661e9ef70c2760
somin1011 <leesomin1011@naver.com>
2021-03-17 11:09:03 +0900

    Add UserController [Update]

--- UserController.java
+++ UserController.java
@@ -6,4 +6,17 @@ public class UserController {
        logger.onfo("경로 : login");
        return "user/login";
    }
+    // 회원가입 페이지로 이동
+   @RequestMapping("/user/insert/do")
+   public String insert() {
+      logger.info("경로 :  insert");
+      return "user/insert"; 
+  }
+
+ // 회원수정 페이지로 이동
+   @RequestMapping("user/update.do")
+    public String update() {
+     logger.info("경로 : update");
+     return "user/update";
+   } 
 }
